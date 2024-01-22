2d517d86b114d3480fe82cb74ce22f9c4cd246eb
jlbuild <juliabuildbot@gmail.com>
2024-01-22 13:57:26 +0000

    gmsh_jll build 4.12.2+0

--- src/wrappers/aarch64-apple-darwin.jl
+++ src/wrappers/aarch64-apple-darwin.jl
@@ -15,7 +15,7 @@ using OCCT_jll
 using Zlib_jll
 JLLWrappers.@generate_wrapper_header("gmsh")
 JLLWrappers.@declare_file_product(gmsh_api)
-JLLWrappers.@declare_library_product(libgmsh, "@rpath/libgmsh.4.11.dylib")
+JLLWrappers.@declare_library_product(libgmsh, "@rpath/libgmsh.4.12.dylib")
 JLLWrappers.@declare_executable_product(gmsh)
 function __init__()
     JLLWrappers.@generate_init_header(Cairo_jll, FLTK_jll, FreeType2_jll, GMP_jll, HDF5_jll, JpegTurbo_jll, libpng_jll, LLVMOpenMP_jll, METIS_jll, MMG_jll, OCCT_jll, Zlib_jll)
@@ -26,7 +26,7 @@ function __init__()
 
     JLLWrappers.@init_library_product(
         libgmsh,
-        "lib/libgmsh.4.11.1.dylib",
+        "lib/libgmsh.4.12.2.dylib",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
