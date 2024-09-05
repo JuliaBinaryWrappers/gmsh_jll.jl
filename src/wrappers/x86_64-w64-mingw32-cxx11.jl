456436f81900a4fe18184dc0407b2d0064469fc0
jlbuild <juliabuildbot@gmail.com>
2024-09-05 22:17:51 +0000

    gmsh_jll build 4.13.1+0

--- src/wrappers/x86_64-w64-mingw32-cxx11.jl
+++ src/wrappers/x86_64-w64-mingw32-cxx11.jl
@@ -14,17 +14,11 @@ using MMG_jll
 using OCCT_jll
 using Zlib_jll
 JLLWrappers.@generate_wrapper_header("gmsh")
-JLLWrappers.@declare_library_product(libgmsh, "gmsh.dll")
 JLLWrappers.@declare_executable_product(gmsh)
 JLLWrappers.@declare_file_product(gmsh_api)
+JLLWrappers.@declare_library_product(libgmsh, "gmsh.dll")
 function __init__()
     JLLWrappers.@generate_init_header(Cairo_jll, CompilerSupportLibraries_jll, FLTK_jll, FreeType2_jll, GMP_jll, HDF5_jll, JpegTurbo_jll, libpng_jll, METIS_jll, MMG_jll, OCCT_jll, Zlib_jll)
-    JLLWrappers.@init_library_product(
-        libgmsh,
-        "bin\\gmsh.dll",
-        RTLD_LAZY | RTLD_DEEPBIND,
-    )
-
     JLLWrappers.@init_executable_product(
         gmsh,
         "bin\\gmsh.exe",
@@ -33,6 +27,12 @@ function __init__()
     JLLWrappers.@init_file_product(
         gmsh_api,
         "lib\\gmsh.jl",
+    )
+
+    JLLWrappers.@init_library_product(
+        libgmsh,
+        "bin\\gmsh.dll",
+        RTLD_LAZY | RTLD_DEEPBIND,
     )
 
     JLLWrappers.@generate_init_footer()
