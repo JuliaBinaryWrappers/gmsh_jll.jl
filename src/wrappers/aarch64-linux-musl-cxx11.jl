61647dfdcf7601439a64fe4671f059cc240e3e9f
jlbuild <juliabuildbot@gmail.com>
2025-11-27 11:27:32 +0000

    gmsh_jll build 4.15.0+0

--- src/wrappers/aarch64-linux-musl-cxx11.jl
+++ src/wrappers/aarch64-linux-musl-cxx11.jl
@@ -7,6 +7,7 @@ using FLTK_jll
 using FreeType2_jll
 using GLU_jll
 using GMP_jll
+using HDF5_jll
 using JpegTurbo_jll
 using Libglvnd_jll
 using libpng_jll
@@ -21,11 +22,17 @@ using Xorg_libXinerama_jll
 using Xorg_libXrender_jll
 using Zlib_jll
 JLLWrappers.@generate_wrapper_header("gmsh")
+JLLWrappers.@declare_library_product(libgmsh, "libgmsh.so.4.15")
 JLLWrappers.@declare_executable_product(gmsh)
 JLLWrappers.@declare_file_product(gmsh_api)
-JLLWrappers.@declare_library_product(libgmsh, "libgmsh.so.4.13")
 function __init__()
-    JLLWrappers.@generate_init_header(Cairo_jll, CompilerSupportLibraries_jll, FLTK_jll, FreeType2_jll, GLU_jll, GMP_jll, JpegTurbo_jll, Libglvnd_jll, libpng_jll, METIS_jll, MMG_jll, OCCT_jll, Xorg_libX11_jll, Xorg_libXext_jll, Xorg_libXfixes_jll, Xorg_libXft_jll, Xorg_libXinerama_jll, Xorg_libXrender_jll, Zlib_jll)
+    JLLWrappers.@generate_init_header(Cairo_jll, CompilerSupportLibraries_jll, FLTK_jll, FreeType2_jll, GLU_jll, GMP_jll, HDF5_jll, JpegTurbo_jll, Libglvnd_jll, libpng_jll, METIS_jll, MMG_jll, OCCT_jll, Xorg_libX11_jll, Xorg_libXext_jll, Xorg_libXfixes_jll, Xorg_libXft_jll, Xorg_libXinerama_jll, Xorg_libXrender_jll, Zlib_jll)
+    JLLWrappers.@init_library_product(
+        libgmsh,
+        "lib/libgmsh.so",
+        RTLD_LAZY | RTLD_DEEPBIND,
+    )
+
     JLLWrappers.@init_executable_product(
         gmsh,
         "bin/gmsh",
@@ -34,12 +41,6 @@ function __init__()
     JLLWrappers.@init_file_product(
         gmsh_api,
         "lib/gmsh.jl",
-    )
-
-    JLLWrappers.@init_library_product(
-        libgmsh,
-        "lib/libgmsh.so",
-        RTLD_LAZY | RTLD_DEEPBIND,
     )
 
     JLLWrappers.@generate_init_footer()
